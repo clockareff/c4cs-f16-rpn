b47cf5b26c3695db47ca5facd45b7d9018c6f9a2
Colin J Lockareff <clockar@caen-vnc-vm14.engin.umich.edu>
2016-10-05 14:26:25 -0400

    Subtract is working

--- rpn.py
+++ rpn.py
@@ -8,6 +8,11 @@ def calculate(string):
 			arg2 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		else if token == '-'
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 - arg2
+			stack.append(result)
 		else
 			stack.append(int(token))
 		print(stack)
